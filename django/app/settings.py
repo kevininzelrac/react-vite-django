0abb534ceb52d2c2a13f56c24e0bb6d8c445029d
Kevin Inzelrac <kevininzelrac@me.com>
2024-02-27 18:34:06 +0100

    setting env in django

--- django/app/settings.py
+++ django/app/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import environ
+env = environ.Env()
+environ.Env.read_env(env_file='./.env')
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
