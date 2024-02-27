88f5f074434f6196d590029edee33ebf95f91b06
Kevin Inzelrac <kevininzelrac@me.com>
2024-02-27 17:47:10 +0100

    update admin.py

--- django/posts/admin.py
+++ django/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin ):
+    fieldsets = [
+        #(None,               {'fields': ['id']}),
+        (None,               {'fields': ['title']}),
+        (None,               {'fields': ['content']}),
+        #(None,               {'fields': ['created_at']}),
+        #(None,               {'fields': ['updated_at']}),
+    ]
+    list_display = ('id','title', 'content', 'created_at', 'updated_at')
+
+admin.site.register(Post, PostAdmin)
