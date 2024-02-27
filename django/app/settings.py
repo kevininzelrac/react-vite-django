7f5127095e58c96dedf2daf226627828038afd16
Kevin Inzelrac <kevininzelrac@me.com>
2024-02-27 19:01:37 +0100

    Update app/settings.py + python manage.py loaddata datadump.json

--- django/app/settings.py
+++ django/app/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
